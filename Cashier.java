183c21db440de26cba49615ff0a7611ab8de2415
nortoha <39999692+nortoha@users.noreply.github.com>
2020-02-06 14:30:41 -0600

    TEACHER NOTES
    please read my teacher comments in your code.

--- Cashier.java
+++ Cashier.java
@@ -4,9 +4,13 @@ public class Cashier extends Employee{
 	
 	void getRegisterNum() {
 	
+	// TEACHER NOTES: don't need this on line 8.  your method below on line 14 is correct.
 	setRegisterNum();
 }
 
+	// TEACHER NOTES: remember a setter method should take a parameter most of the time:
+	// private void setRegisterNum(int num){
+	// }
 	private void setRegisterNum() {
 		
 	}
